55d1c7e2d5cdf886284aa3900ed4d13e5305c80c
LuxerIThink <luxerthegamer@gmail.com>
2023-05-07 11:55:49 +0200

    Remove Shared folder bind mount (volume)

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-loc="/home/${USER}/Shared/RiSA_Ros2Humble"
-mkdir -p $loc
 xhost +local:root
-docker run -it --rm \
-	--privileged \
-	--name=ros2humble \
-	--shm-size=1g \
-	--ulimit memlock=-1 \
-	-e DISPLAY=$DISPLAY \
-	-e XDG_RUNTIME_DIR=/tmp \
-	-e QT_X11_NO_MITSHM=1 \
-        -v /tmp/.X11-unix:/tmp/.X11-unix \
-	-v $loc:/root/Shared/ros2_ws \
-	--device=/dev/dri:/dev/dri \
-	--device=/dev/video0 \
-	--net=host \
-	ros2humble \
-	kitty
+# shellcheck disable=SC2046
+# shellcheck disable=SC2215
+# shellcheck disable=SC2006
+docker run -it --rm\
+        --name=ros2humble \
+        --shm-size=1g \
+        --ulimit memlock=-1 \
+        -e DISPLAY=$DISPLAY \
+        -e XDG_RUNTIME_DIR=/tmp \
+        -e QT_X11_NO_MITSHM=1 \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v /usr/bin/docker:/usr/bin/docker \
+        --device=/dev/dri:/dev/dri \
+        --device=/dev/video0 \
+        --network=host \
+        ros2humble \
+        bash
