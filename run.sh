8c85b62c0a701e258059311a8552c83100c9d1e5
LuxerIThink <eliteluxer@gmail.com>
2023-05-29 01:53:41 +0200

    Fix run.sh for rootless docker

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 xhost +local:root
-# shellcheck disable=SC2046
-# shellcheck disable=SC2215
-# shellcheck disable=SC2006
 docker run -it --rm\
         --name=ros2humble \
         --shm-size=1g \
-        --ulimit memlock=-1 \
         -e DISPLAY=$DISPLAY \
         -e XDG_RUNTIME_DIR=/tmp \
         -e QT_X11_NO_MITSHM=1 \
